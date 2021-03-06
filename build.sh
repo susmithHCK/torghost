017b594cdeb628a66f6b41155acecd8fd17a2a8a
Tsvetan Gerov <tsvetan@gerov.eu>
2021-03-06 18:43:37 +0200

    Detect Python3 version

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 echo "Torghost installer v3.0"
 echo "Installing prerequisites "
-sudo apt-get install tor python3-pip -y 
+sudo apt-get install tor python3-pip python3-dev -y 
 echo "Installing dependencies "
 sudo pip3 install -r requirements.txt 
+PYVER=$(python3 --version | awk '{print$2}' | cut -f 1,2 -d .)
 mkdir build
 cd build
 cython3 ../torghost.py --embed -o torghost.c --verbose
@@ -12,7 +13,7 @@ else
     echo [ERROR] Build failed. Unable to generate C code using cython3
     exit 1
 fi
-gcc -Os -I /usr/include/python3.8 -o torghost torghost.c -lpython3.8 -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python${PYVER} -o torghost torghost.c -lpython${PYVER} -lpthread -lm -lutil -ldl
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Compiled to static binay 
 else
